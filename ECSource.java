89f411d8bb506ecee071c91355d245d1970a2da9
Katta <manibhargavi.katta@fiserv.com>
2023-11-06 14:58:11 -0500

    finished

--- ECSource.java
+++ ECSource.java
@@ -9,17 +9,26 @@ class Node<T> {
 }
 
 class ECSource {
-  public static <T> boolean isUnivalueList(Node<T> head) {
+  public static <T> boolean isUnivalueListR(Node<T> head) {
     // todo
     if(head.next==null) return true;  
-    return (head.next.val.equals(head.val)) ? isUnivalueList(head.next) : false; 
+    return (head.next.val.equals(head.val)) ? isUnivalueListR(head.next) : false; 
+  }
+
+  public static <T> boolean isUnivalueList(Node<T> head) {
+    // todo
+    while(head!=null){
+      if (head.next!=null && !head.next.val.equals(head)) return false; 
+      head = head.next; 
+    }
+    return true; 
   }
 
   public static void main(String[] args) {
-  /*   Node<Integer> u = new Node<>(2);
+     Node<Integer> u = new Node<>(2);
   Node<Integer> v = new Node<>(2);
-  Node<Integer> w = new Node<>(3);
-  Node<Integer> x = new Node<>(3);
+  Node<Integer> w = new Node<>(2);
+  Node<Integer> x = new Node<>(2);
   Node<Integer> y = new Node<>(2);
 
   u.next = v;
@@ -29,11 +38,11 @@ class ECSource {
 
   // 2 -> 2 -> 3 -> 3 -> 2
 
-  System.out.println(ECSource.isUnivalueList(u)); // false*/
+  System.out.println(ECSource.isUnivalueList(u)); // false
 
-    Node<String> z = new Node<>("z");
+  //  Node<String> z = new Node<>("z");
   // z
   
-    System.out.println(ECSource.isUnivalueList(z)); // true
+   // System.out.println(ECSource.isUnivalueList(z)); // true
   }
 }
